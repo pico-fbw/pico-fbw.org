85805b5e662410051416a97d67ed1f88c123885e
Camden Rush <rush0camden@gmail.com>
2023-07-14 18:01:33 -0500

    fix: use NavLink

--- src/Index.tsx
+++ src/Index.tsx
@@ -4,8 +4,8 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import { NavLink } from 'react-router-dom';
 
 const navigation = [
-    { name: 'Home', href: '/' },
-    { name: 'Flight Planner', href: '/planner' },
+    { name: 'Home', to: '/' },
+    { name: 'Flight Planner', to: '/planner' },
 ];
 
 export default function Index() {
@@ -76,12 +76,15 @@ export default function Index() {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map(item => (
-                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
+                            <NavLink
+                                key={item.name}
+                                to={item.to}
+                                className="text-sm font-semibold leading-6 text-white"
+                            >
                                 {item.name}
-                            </a>
+                            </NavLink>
                         ))}
                     </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
                 </nav>
                 <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                     <div className="fixed inset-0 z-50" />
@@ -149,13 +152,13 @@ export default function Index() {
                             <div className="-my-6 divide-y divide-gray-500/25">
                                 <div className="space-y-2 py-6">
                                     {navigation.map(item => (
-                                        <a
+                                        <NavLink
                                             key={item.name}
-                                            href={item.href}
+                                            to={item.to}
                                             className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                                         >
                                             {item.name}
-                                        </a>
+                                        </NavLink>
                                     ))}
                                 </div>
                                 <div className="py-6"></div>
